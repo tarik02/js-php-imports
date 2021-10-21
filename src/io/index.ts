16d954a3c4ef6612c0746bb6bbc9b70693ebbf62
Tarik02 <Taras.Fomin@gmail.com>
2021-10-21 11:27:40 +0300

    Fix config error reporting

--- src/io/index.ts
+++ src/io/index.ts
@@ -1,14 +1,29 @@
 import * as Either from 'fp-ts/Either'
+import { Errors } from 'io-ts'
+import { formatValidationErrors } from 'io-ts-reporters'
 
 import { PhpImportsConfig } from './PhpImportsConfig'
 import { PhpImportsRc } from './PhpImportsRc'
 
 
+export class ConfigParsingError extends Error {
+	errors: Errors
+	source: any
+
+	// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+	constructor(errors: Errors, source: any) {
+		super(`${formatValidationErrors(errors)}\nConfig:\n${JSON.stringify(source, undefined, '  ')}`)
+
+		this.errors = errors
+		this.source = source
+	}
+}
+
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const parseConfigFromObject = (source: any): PhpImportsConfig => {
 	const result = PhpImportsConfig.decode(source)
 	if (Either.isLeft(result)) {
-		throw result.left
+		throw new ConfigParsingError(result.left, source)
 	}
 
 	return result.right
@@ -18,7 +33,7 @@ export const parseConfigFromObject = (source: any): PhpImportsConfig => {
 export const parseRcFromObject = (source: any): PhpImportsRc => {
 	const result = PhpImportsRc.decode(source)
 	if (Either.isLeft(result)) {
-		throw result.left
+		throw new ConfigParsingError(result.left, source)
 	}
 
 	return result.right
